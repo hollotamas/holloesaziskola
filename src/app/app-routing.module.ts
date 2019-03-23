9e7988ec3f2094b65cddb28c58a8e69baf3d8879
unknown <corvus@HT-notebook.batsanyi.local>
2019-03-23 21:19:02 +0100

    Alert component és Service és page not found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {path: '', component: AppComponent },
+  //{path: '', redirectTo: '/tartalom', pathMatch: 'full' },
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AlertComponent } from './core/alert/alert.component';
+import { AlertService } from './shared/alert.service';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlertComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AlertService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
